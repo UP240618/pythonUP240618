f9aeae52f1852d71f9ba2686b34d3b76c0f7a381
up240618 <up240618@alumnos.upa.edu.mx>
2025-03-10 13:29:21 -0600

    Delete Ej_1.py

--- Ej_1.py
+++
@@ -1,9 +0,0 @@
-print(3 + 4)
-print(3 - 4)
-print(3 * 4)
-print(3 % 9)
-print(3 / 4)
-print(3 ** 4)
-print(3 // 4)
-
-print("Jaime, Alonso ", "Moreno Zapata, ", "Mexico, ", "I'am enjoying 30 days of python.")
